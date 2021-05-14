f66f4c670d42eefe7821c2ea1b1565dc24988dce
Ananya-Krishnappa <ananyak3395@gmail.com>
2021-05-14 13:08:11 +0530

    Added:empwage using dictionary

---
+++ empWageDict.sh
@@ -0,0 +1,41 @@
+#!/bin/bash -x 
+isPartTime=1
+isFullTime=2
+maxHrsInMonth=10
+empRatePerHr=20
+numofWorkingDays=20
+totalEmpHr=0
+totalWorkingDays=0
+
+declare -A empDailyWage
+
+function getWorkingHours() {
+	case $1 in
+		$isPartTime)
+				workhrs=8
+				;;
+		$isFulltime)
+				workhrs=4
+				;;
+		*)
+			workhrs=0
+			;;
+	esac
+	echo $workhrs
+}
+function calculatewage(){
+	local workhrs=$1
+	wage=$(($workhrs*$empRatePerHr))
+	echo $wage
+} 
+while [[ $totalEmpHr -lt $maxHrsInMonth && $totalWorkingDays -lt $numofWorkingDays ]]
+do
+	((totalWorkingDays++))
+	empCheck=$((RANDOM%3)) 
+	workhrs="$(getWorkingHours $empCheck)"
+	totalEmpHr=$(($totalEmpHr+$workhrs))
+	empDailyWage["$totalWorkingDays"]="$(calculatewage $workhrs)"
+done
+totalSalary="$(calculatewage $totalEmpHr)"
+echo Daily wage: ${empDailyWage[@]}
+echo all keys: ${!empDailyWage[@]}
